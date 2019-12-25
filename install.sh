313ad63dc6420280ff4804b6eee91d4811305a04
Yuma Matsune <yuma.matsune@gmail.com>
2019-12-25 12:57:52 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME
 git clone --recursive git@github.com:matsune/dotfiles.git
-ln -s .zshrc ./dotfiles/zsh/.zshrc
-ln -s .vimrc ./dotfiles/vim/.vimrc
-ln -s .tmux.conf ./dotfiles/tmux/.tmux.conf
+ln -s ./dotfiles/zsh/.zshrc ~/.zshrc 
+ln -s ./dotfiles/vim/.vimrc ~/.vimrc
+ln -s ./dotfiles/tmux/.tmux.conf ~/.tmux.conf
