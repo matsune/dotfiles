a81b4506e366383f358837d751066355ae660b50
Yuma Matsune <yuma.matsune@gmail.com>
2020-01-14 01:03:30 +0900

    add .gitconfig

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone --recursive https://github.com/matsune/dotfiles.git
 ln -f -s ./dotfiles/zsh/.zshrc ~/.zshrc 
 ln -f -s ./dotfiles/vim/.vimrc ~/.vimrc
 ln -f -s ./dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -f -s ./dotfiles/git/.gitconfig ~/.gitconfig
