0db038dd1de1e107a1d426d821053d318fccd8dd
Yuma Matsune <yuma.matsune@gmail.com>
2020-09-07 23:17:46 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -eu
+
+function ask_override() {
+  if [ -f $2 ]; then
+    if [ -L $2 ]; then
+      read -p "Do you wish to override $2? [yN] " yn
+      case $yn in
+          [Yy]* ) ;; 
+          * ) return;;
+      esac
+    fi
+  fi
+  echo "ln -f -s $1 $2" 
+  ln -f -s $1 $2
+}
+
 cd $HOME
-git clone --recursive https://github.com/matsune/dotfiles.git
-ln -f -s ./dotfiles/zsh/.zshrc ~/.zshrc 
-ln -f -s ./dotfiles/vim/.vimrc ~/.vimrc
-ln -f -s ./dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -f -s ./dotfiles/git/.gitconfig ~/.gitconfig
-ln -f -s ./dotfiles/tig/.tigrc ~/.tigrc
+[ ! -d "dotfiles" ] && git clone --recursive https://github.com/matsune/dotfiles.git
+ask_override ./dotfiles/zsh/.zshrc ~/.zshrc 
+ask_override ./dotfiles/vim/.vimrc ~/.vimrc
+ask_override ./dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ask_override ./dotfiles/git/.gitconfig ~/.gitconfig
+ask_override ./dotfiles/tig/.tigrc ~/.tigrc
